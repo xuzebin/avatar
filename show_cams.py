46788bdb6f60bc865789c1226c75b0a620110a8f
xuzebin <zebinxu7@gmail.com>
2017-04-03 16:41:42 -0400

    update show_cams

--- show_cams.py
+++ show_cams.py
@@ -1,16 +1,39 @@
 #!/usr/bin/env python
 
 import cv2
+import numpy as np
 
+left_cam_id = 2
+right_cam_id = 1
 width = 680
 height = 480
 
-def show_cameras(left, right):
+def show_camera(cam_id, width, height):
+    """
+    Show one camera
+    """
+    cap = cv2.VideoCapture(cam_id)
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
+    
+    while True:
+        ret, img = cap.read()
+        if ret:
+            cv2.imshow('single camera', img)
+             
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+    
+
+def show_cameras(left, right, width, height):
 
     """
-    Show two cameras in separate windows
+    Show two cameras in a horizontal aligned window
     """
-
+    
     cap_left = cv2.VideoCapture(left)
     cap_left.set(cv2.CAP_PROP_FRAME_WIDTH, width)
     cap_left.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
@@ -19,20 +42,12 @@ def show_cameras(left, right):
     cap_right.set(cv2.CAP_PROP_FRAME_WIDTH, width)
     cap_right.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
 
-    cv2.namedWindow("left")
-    cv2.namedWindow("right")
-    # Align left and right windows
-    cv2.moveWindow("left", 50, 150);
-    cv2.moveWindow("right", width + 50, 150);
-
     while True:
         ret1, img1 = cap_left.read()
         ret2, img2 = cap_right.read()
-        # Wait until ret1/ret2 is true since camera setup needs some time.
-        if ret1:
-            cv2.imshow('left', img1)
-        if ret2:
-            cv2.imshow('right', img2)
+        # Wait until ret1 and ret2 are true since camera setup needs some time.
+        if ret1 and ret2:
+            cv2.imshow("stereo cameras", np.hstack((img1, img2)))
              
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
@@ -42,4 +57,5 @@ def show_cameras(left, right):
     cv2.destroyAllWindows()
 
 if __name__ == "__main__":
-    show_cameras(2, 1)
+#    show_cameras(left_cam_id, right_cam_id, width, height)
+    show_camera(0, width, height)
