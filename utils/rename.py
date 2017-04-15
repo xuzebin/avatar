b1b5ef3f0fb8286afb758b5072e383390b123b43
xuzebin <zebinxu7@gmail.com>
2017-04-14 23:02:15 -0400

    update rename script

--- utils/rename.py
+++ utils/rename.py
@@ -2,7 +2,7 @@
 
 """
 A script to rename a set of stereo image pairs so that their names are in a consecutive order (eg. 0L.jpg, 0R.jpg, 1L.jpg, 1R.jpg, etc).
-When some chessboard pairs contain outliers, we remove those image pairs, and run this script to reorder.
+When some chessboard pairs contain outliers, we remove those image pairs, and run this script to reorder to  make the file indices consecutive.
 """
 
 import os
@@ -10,15 +10,15 @@ import argparse
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Rename files in a directory. The files must be in the format: %dL.jpg, %dR.jpg, where %d is a number.')
-    parser.add_argument('-start_index', metavar='start_index', type=int,
-                        help='starting index')
+    parser.add_argument('start_index', metavar='start_index', type=int,
+                        help='start index of the image pair')
     parser.add_argument('end_index', metavar='end_index', type=int,
-                        help='ending index')
+                        help='end index of the image pair')
     parser.add_argument('directory', help='the directory containing the files to be renamed')
     args = parser.parse_args()
 
     count = 0
-    for x in range(args.end_index + 1):
+    for x in range(args.start_index, args.end_index + 1):
         left_filename = os.path.join(args.directory, "{0}L.jpg".format(x))
         right_filename = os.path.join(args.directory, "{0}R.jpg".format(x))
 
