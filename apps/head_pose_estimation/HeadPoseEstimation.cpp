f664dc793461042a361c59f396b3cfd56b38ba56
xuzebin <zebinxu7@gmail.com>
2017-05-11 03:50:32 -0400

    move unnecessary allocation out of loop

--- apps/head_pose_estimation/HeadPoseEstimation.cpp
+++ apps/head_pose_estimation/HeadPoseEstimation.cpp
@@ -25,6 +25,17 @@ bool showFps = true;
 bool startTracking = true;
 bool startSending = false;
 
+// World points for pose estimation
+void constructWorldPoints(std::vector<cv::Point3f>& modelPoints) {
+    modelPoints.reserve(6);
+    modelPoints.push_back(cv::Point3f(0, 0, 0));//nose
+    modelPoints.push_back(cv::Point3f(0, 11, 3.5));//jaw
+    modelPoints.push_back(cv::Point3f(-5.5, -6, 3.5));//left corner of left eye
+    modelPoints.push_back(cv::Point3f(5.5, -6, 3.5));//right corner of right eye
+    modelPoints.push_back(cv::Point3f(-2.5, 3.5, 3));//left corner of mouth
+    modelPoints.push_back(cv::Point3f(2.5, 3.5, 3));//right corner of mouth
+}
+
 int main(int argc, char** argv) {
     try {
         if (argc >= 5 || argc == 3) {
@@ -58,9 +69,50 @@ int main(int argc, char** argv) {
             std::cout << width << "x" << height << std::endl;
         }
 
-
         cv::Size size(640, 360);
         std::cout << "resized to: " << size << std::endl;
+
+        // Preallocation
+        
+        // Rotation and translation vectors
+        cv::Mat rvec(3,1,cv::DataType<double>::type);
+        cv::Mat tvec(3,1,cv::DataType<double>::type);
+
+        double GLCoord[9] = {1,0,0,
+                             0,1,0,
+                             0,0,1};
+
+        // to rotation matrix
+        Rodrigues(cv::Mat(3, 3, CV_64FC1, GLCoord), rvec);
+
+        // Landmark positions used for pose estimation
+        int poseFaceIds[6] = {33, 8, 36, 45, 48, 54};
+
+        // Axes to draw
+        std::vector<cv::Point2f> image_axes(3);
+        std::vector<cv::Point3f> world_axes = {cv::Point3f(10, 0, 0),
+                                               cv::Point3f(0, 10, 0), 
+                                               cv::Point3f(0, 0, -10)};
+
+        // Estimate camera's intrinsic matrix
+        double focalLength = size.width;
+        cv::Point2d center(size.width / 2, size.height / 2);
+        cv::Mat cameraMatrix = (cv::Mat_<double>(3, 3) << 
+                                focalLength, 0.0, center.x,
+                                0.0, focalLength, center.y,
+                                0.0, 0.0, 1.0);
+
+        cv::Mat distCoeffs(4,1,cv::DataType<double>::type);
+        distCoeffs.at<double>(0) = 0;
+        distCoeffs.at<double>(1) = 0;
+        distCoeffs.at<double>(2) = 0;
+        distCoeffs.at<double>(3) = 0;
+
+        // Landmark positions in a right-hand coordinate 3d world frame, centered at nose
+        std::vector<cv::Point3f> modelPoints;
+        constructWorldPoints(modelPoints);
+
+        std::vector<cv::Point2f> subLandmarks1(6);
 
         double startTime = 0.0;
         // Grab and process frames until the main window is closed by the user.
@@ -86,49 +138,20 @@ int main(int argc, char** argv) {
                 const std::vector<cv::Point2f> p1 = tracker.getFaceLandMarks();
 
                 // Estimate pose
-                cv::Mat rvec(3,1,cv::DataType<double>::type);
-                cv::Mat tvec(3,1,cv::DataType<double>::type);
-
-                double GLCoord[9] = {1,0,0,
-                                     0,1,0,
-                                     0,0,1};
-
-                Rodrigues(cv::Mat(3, 3, CV_64FC1, GLCoord), rvec);
-                tvec.at<double>(0, 0) = 0; tvec.at<double>(1, 0) = 0; tvec.at<double>(2, 0) = 1;
-                
-                cv::Mat distCoeffs(4,1,cv::DataType<double>::type);
-                distCoeffs.at<double>(0) = 0;//-0.41979;
-                distCoeffs.at<double>(1) = 0;//0.206977;
-                distCoeffs.at<double>(2) = 0;
-                distCoeffs.at<double>(3) = 0;
-
-                // Estimate camera's intrinsic matrix
-                double focalLength = size.width;
-                cv::Point2d center(size.width / 2, size.height / 2);
-                cv::Mat cameraMatrix = (cv::Mat_<double>(3, 3) << 
-                                        focalLength, 0.0, center.x,
-                                        0.0, focalLength, center.y,
-                                        0.0, 0.0, 1.0);
-
-                std::vector<cv::Point3f> modelPoints(6);
-                modelPoints[0] = cv::Point3f(0, 0, 0);//nose
-                modelPoints[1] = cv::Point3f(0, 11, 3.5);//jaw
-                modelPoints[2] = cv::Point3f(-5.5, -6, 3.5);//left eye brow
-                modelPoints[3] = cv::Point3f(5.5, -6, 3.5);//right eye brow
-                modelPoints[4] = cv::Point3f(-2.5, 3.5, 3);//left mouth
-                modelPoints[5] = cv::Point3f(2.5, 3.5, 3);//right mouth
-
-
-                int poseFaceIds[6] = {33, 8, 36, 45, 48, 54};
-                std::vector<cv::Point2f> subLandmarks1(6);
                 for (int i = 0; i < 6; ++i) {
                     subLandmarks1[i] = p1[poseFaceIds[i]];
                 }
+
+                // Must be reset to an initial guess for solvePnP to work properly.
+                tvec.at<double>(0, 0) = 0;
+                tvec.at<double>(1, 0) = 0;
+                tvec.at<double>(2, 0) = 1;
+                
                 cv::solvePnP(modelPoints,
                              subLandmarks1,
                              cameraMatrix,
                              distCoeffs,
-                             rvec, tvec, true, CV_ITERATIVE);//CV_EPNP);CV_ITERATIVE);
+                             rvec, tvec, true, CV_ITERATIVE);
                 //                std::cout << "rotation: " << rvec << std::endl;
                 std::cout << "translation: " << tvec << std::endl;
 
@@ -142,34 +165,32 @@ int main(int argc, char** argv) {
                                          rotMat.at<double>(2, 0), rotMat.at<double>(2, 1), rotMat.at<double>(2, 2), tvec.at<double>(2, 0),
                                          0, 0, 0, 1);
 
-                float x, y, z;
-                rotationMatrixToEulerAngles(rotMat, x, y, z);
+//                 float x, y, z;
+//                 rotationMatrixToEulerAngles(rotMat, x, y, z);
+
 
                 // Project 3d points back to image points
-                std::vector<cv::Point2f> projectedPoints;
+                std::vector<cv::Point2f> projectedPoints(modelPoints.size());
                 cv::projectPoints(modelPoints, rvec, tvec, cameraMatrix, distCoeffs, projectedPoints);
+
                 //Draw projected corners
                 for(int i = 0; i < projectedPoints.size(); ++i) {
                     cv::circle(face, projectedPoints[i], 2, cv::Scalar(0, 255, 255), -1);
                 }
 
-                // Draw X,Y,Z axes
-                std::vector<cv::Point2f> image_axes;
-                static std::vector<cv::Point3f> axes = {cv::Point3f(10, 0, 0),
-                                                        cv::Point3f(0, 10, 0), 
-                                                        cv::Point3f(0, 0, -10)};
 
-                cv::projectPoints(axes, rvec, tvec, cameraMatrix, distCoeffs, image_axes);
 
-                cv::arrowedLine(face, p1[33], image_axes[0], cv::Scalar(0, 0, 255), 2);
-                cv::arrowedLine(face, p1[33], image_axes[1], cv::Scalar(0, 255, 0), 2);
-                cv::arrowedLine(face, p1[33], image_axes[2], cv::Scalar(255, 0, 0), 2);
+                // Draw X,Y,Z axes that are reprojected back to the imaegs
+                cv::projectPoints(world_axes, rvec, tvec, cameraMatrix, distCoeffs, image_axes);
+
+                cv::line(face, p1[33], image_axes[0], cv::Scalar(0, 0, 255), 2);
+                cv::line(face, p1[33], image_axes[1], cv::Scalar(0, 255, 0), 2);
+                cv::line(face, p1[33], image_axes[2], cv::Scalar(255, 0, 0), 2);
                 
                 // Send data to server
                 if (startSending) {
                     socketClient.sendModelMatrix2Avatar(projectionMat);
                 }
-
             }
 
             // Flip
